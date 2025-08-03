fe09bbfe6d7514b8b68dc8120ac83312495e2ace
LhordPAIN7 <titagabriel677@gmail.com>
2025-08-03 13:15:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'TOHID-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '918930358452' ;  // put your number here
+global.pairingNumber = process.env.BOT_NUMBER || '237677191716' ;  // put your number here
 global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
 
 global.mods = []
